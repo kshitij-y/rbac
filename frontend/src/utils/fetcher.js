b170f645353a77a662775906bf4a15aa3bf2c71e
Kshitij kumar <kshitijyadav2003@gmail.com>
2025-07-04 18:12:32 +0530

    minor changes

--- frontend/src/utils/fetcher.js
+++ frontend/src/utils/fetcher.js
@@ -1,4 +1,6 @@
-const uri = import.meta.env.VITE_API_URL;
+const uri =
+  import.meta.env.VITE_API_URL ||
+  "http://ec2-13-61-14-231.eu-north-1.compute.amazonaws.com:3000";
 
 async function fetcher(endpoint, options = {}) {
   if (!uri) {
