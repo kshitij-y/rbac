f733e0e613f9721945248e9b9a7be73c7a3d2796
Kshitij kumar <kshitijyadav2003@gmail.com>
2025-07-04 21:42:52 +0530

    fixed cookie for http

--- backend/src/controller/auth.signout.controller.js
+++ backend/src/controller/auth.signout.controller.js
@@ -1,16 +1,21 @@
 const { sendResponse } = require("../utils/sendResponse");
 
 const signout = (req, res) => {
-  res.setHeader(
-    "Set-Cookie",
-    "token=; HttpOnly; Path=/; Max-Age=0; SameSite=None; Secure"
-  );
-  
-    return sendResponse(res, {
-        status: 200,
-        success: true,
-        message: "Signed out"
-    });
-}
+  const isProd = process.env.NODE_ENV === "production";
+
+  res.cookie("token", "", {
+    httpOnly: true,
+    secure: isProd,
+    sameSite: isProd ? "None" : "Strict",
+    maxAge: 0,
+  });
+
+
+  return sendResponse(res, {
+    status: 200,
+    success: true,
+    message: "Signed out",
+  });
+};
 
 module.exports = signout;
